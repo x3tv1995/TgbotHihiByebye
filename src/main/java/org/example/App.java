3b97b7bb1a7920508a8df7238cf83d768ba99d1e
x3tv1995 <145342836+x3tv1995@users.noreply.github.com>
2024-10-28 19:19:43 +0300

    update

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -11,7 +11,7 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 public class App 
 {
     public static void main( String[] args ) throws TelegramApiException {
-        Bot bot = new Bot("7295275822:AAEBC_--iq_ZNi2O3vf00RWK0R2-3tfzy24");
+        Bot bot = new Bot("");
 
         TelegramBotsApi telegramBot = new TelegramBotsApi(DefaultBotSession.class);
         telegramBot.registerBot(bot);
